0de6bd183749bf488551a0fddca1f82eed4f5a25
Moolvie <Moolvie@gmail.com>
2018-04-17 20:33:01 -0400

    Initial drafts of user login and verification scripts

---
+++ RegisterUser.php
@@ -0,0 +1,151 @@
+<?php
+$Body = "";
+// #3
+$errors = 0;
+$email = "";
+if (empty($_POST['email'])) {
+	++$errors;
+	$Body .= "<p>You need to enter an e-mail address.</p>\n";
+}
+else {
+     $email = stripslashes($_POST['email']);
+     if (preg_match("/^[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*@" .
+	"[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$/i" ,
+	$email) == 0) {
+	++$errors;
+	$Body .= "<p>You need to enter a valid " .
+		"e-mail address.</p>\n";
+	$email = "";
+     }
+}
+// #4
+if (empty($_POST['password'])) {
+	++$errors;
+	$Body .= "<p>You need to enter a password.</p>\n";
+	$password = "";
+}
+else
+	$password = stripslashes($_POST['password']);
+if (empty($_POST['password2'])){
+	++$errors;
+	$Body .= "<p>You need to enter a confirmation password.</p>\n";
+	$password2 = "";
+}
+else 
+	$password2 = stripslashes($_POST['password2']);
+if ((!(empty($password))) && (!(empty($password2)))) {
+	if (strlen($password) < 6) {
+		++$errors;
+		$Body .= "<p>The password is too short.</p>\n";
+		$password = "";
+		$password2 = "";
+	}
+	if ($password <> $password2) {
+		++$errors;
+		$Body .= "<p>The paswords do not match.</p>\n";
+		$password = "";
+		$password2 = "";
+	}
+}
+// #5
+if ($errors == 0) {
+	$DBConnect = @mysqli_connect("localhost", "root", "!root");
+	if ($DBConnect === FALSE) {
+		$Body .= "<p>Unable to connect to the database server" . 
+		"Error code " . mysqli_errno() . ": " . mysqli_error() . "</p>\n";
+		++$errors;
+	}
+	else {
+		$DBName = "Songs";
+		$result = @mysqli_select_db($DBConnect, $DBName);
+		if ($result === FALSE) {
+			$Body .= "<p>Unable to select the database. " . 
+		       	"Error code " . msqli_errno($DBConnect) .
+			": " . mysqli_error($DBConnect) . "</p>\n";
+			++$errors;
+		}
+	}
+}
+// #6
+$TableName = "customer";
+if ($errors == 0) {
+	$SQLstring = "SELECT count(*) FROM $TableName" . " WHERE email='" . $email. "'";
+	$QueryResult = @mysqli_query($DBConnect, $SQLstring);
+	if ($QueryResult !== FALSE) {
+		$Row = mysqli_fetch_row($QueryResult);
+		if ($Row[0]>0) {
+			$Body .= "<p>The email address entered (" .
+			htmlentities($email) . 
+			") is already registered.</p>\n";
+			++$errors;
+		}
+	}
+}
+// #7
+if ($errors > 0) {
+	$Body .= "<p>Please use your browser's BACK button to return" . 
+		" to the form and fix the errors indicated.</p>\n";
+}
+// #8
+if ($errors == 0) {
+	$first = stripslashes($_POST['first']);
+	$last = stripslashes($_POST['last']);
+	$SQLstring = "INSERT INTO $TableName " .
+		"(first, last, email, password) " .
+		" VALUES ( '$first', '$last', '$email', " .
+		" '" . md5($password) . "')";
+	$QueryResult = @mysqli_query($DBConnect, $SQLstring);
+	if ($QueryResult !== FALSE) {
+		$Body .= "<p>Unable to save your registration " .
+			" information. Error code " .
+			mysqli_errno($DBConnect) . ": " .
+			mysqli_error($DBConnect) . "</p>\n";
+		++$errors;
+	}
+	else {
+		$customerID = mysqli_insert_id($DBConnect);
+	}
+	setcookie("customerID", $customerID);
+	mysqli_close($DBConnect);
+}
+// #9
+if ($errors == 0) {
+	$CustomerName = $first . " " . $last;
+	$Body .= "<p>Thank you, $CustomerName. ";
+	$Body .= "Your new Customer ID is <strong>" .
+		$CustomerID . "</strong>.</p>\n";
+}
+if ($errors == 0) {
+	$Body .= "<form method='post' " .
+	" action='AvailableSongs.php'>\n";
+	$Body .= "<input type='hidden' name='customerID' " .
+	" value='$InternID'>\n";
+	$Body .= "<input type='submit' name='submit' " .
+	" value='View Available Songs'>\n";
+	$Body .= "</form>\n";
+	}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<!--
+    
+      Author: Doug Enos	
+      Date:   April 14, 2018
+
+      Filename:RegisterUser.php
+   -->
+<!-- #1 -->
+	<title>User Registration</title>
+</head>
+<body>
+<!-- #2. -->
+<h1>Songs</h1>
+<h2>User Registration</h2>
+<?php
+echo $Body;
+?>
+</body>
+</html>
---
+++ VerifyLogin.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<!--
+      
+      Author: Doug Enos	
+      Date:   Apr 17, 2018
+
+      Filename: VerifyLogin.php
+   -->
+<title>Verify User Login</title>
+<meta http-equiv="content-type" content="text/html; charset=iso-8849-1" />
+</head>
+<body>
+<h1>Songs</h1>
+<h2>Verify User Login</h2>
+<?php
+$errors = 0;
+$DBConnect = @mysqli_connect("localhost", "root", "!root");
+if ($DBConnect === FALSE) {
+    echo "<p>Unable to connect to the database server. " .
+         "Error code " . mysqli_errno() . ": " .
+         mysqli_error() . "</p>\n";
+    ++$errors;
+    }
+    else {
+         $DBName = "Songs";
+         $result = @mysqli_select_db($DBConnect, $DBName);
+		if ($result === FALSE) {
+			echo "<p>Unable to select the database. " .
+				"Error code " . mysqli_errno($DBConnect) .
+				": " . mysqli_error($DBConnect) .
+				"</p>\n";
+		++$errors;
+		}
+	}
+$TableName = "Customer";
+$userEmail = stripslashes($_POST['email']);
+$userPasswd = md5(stripslashes($_POST['password']));
+if ($errors == 0) {
+    $SQLstring = "SELECT customerID, first, last FROM $TableName 
+	 WHERE email='$userEmail' AND password='$userPasswd'";
+    $QueryResult = @mysqli_query($DBConnect, $SQLstring);
+    if (mysqli_num_rows($QueryResult)==0) {
+        echo "<p>The e-mail address/password " .
+            " combination entered is not valid.
+            </p>\n";
+    ++$errors;
+    }
+    else {
+        $Row = mysqli_fetch_assoc($QueryResult);
+        $CustomerID = $Row['customerID'];
+        $CustomerName = $Row['first'] . " " .
+        $Row['last'];
+        echo "<p>Welcome back, $CustomernName!</p>\n";
+    }
+}
+if ($errors > 0) {
+    echo "<p>Please use your browser's BACK button
+        to return " .
+        " to the form and fix the errors
+        indicated.</p>\n";
+}
+if ($errors == 0) {
+	echo "<p><a href='AvailableSongs.php?" .
+		"customerID=$customerID'>Available " .
+		" Songs</a></p>\n";
+}
+?>
+</body>
+</html>
