a4b1deb144280577f1e5c7f9a3761c682006b94b
Moolvie <Moolvie@gmail.com>
2018-04-18 06:15:54 -0400

    more cleanup

--- UserLogin.php
+++ UserLogin.php
@@ -2,18 +2,18 @@
 	 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>College Internships</title>
+<title>User Login</title>
 <meta http-equiv="content-type" content="text/html; charset=iso-8849-1" />
 </head>
 <body>
 <h1>College Internships</h1>
 <h2>Register / Log In</h2>
-<p>New interns, please complete the top form to
+<p>New users, please complete the top form to
 register as a user. Returning users, please complete
 the second form to log in.</p>
 <hr />
 <h3>New Intern Registration</h3>
-<form method="post" action="RegisterIntern.php">
+<form method="post" action="RegisterUser.php">
 <p>Enter your name: First
 <input type="text" name="first" />
 Last:
